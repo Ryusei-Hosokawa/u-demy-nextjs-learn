9085e23deaf141a013a150def712f031e191c9d0
Ryusei-Hosokawa <korokoro.seika@gmail.com>
2024-06-28 00:55:42 +0900

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        darkblue: "00008b",
+        cadetblue: "5f9ea0",
+        whitesmoke: "f5f5f5",
+      },
+    },
+  },
+  plugins: [],
+};
